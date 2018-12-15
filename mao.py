ff5bceda28fc21ae4579d89bcf6436e354228fe9
sunalter <1401505483@qq.com>
2018-12-15 23:33:01 +0800

    delete

--- mao.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-url = 'http://placekitten.com/100/200'
-respones = urllib.request.urlopen(url)
-cat_img = respones.read()
-with open('cat_100_200.jpg','wb')as f:
-        f.write(cat_img)
--- 新建文本文档.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-im = Image.open('cat_100_200.jpg')
-w,h = im.size
-print('Original image size: %s%s' %(w,h))
-im.thumbnail((w//2,h//2))
-print('Resize image to : %s%s' % (w//2,h//2))
-im.save('thumbnail.jpg','jpeg')
-
